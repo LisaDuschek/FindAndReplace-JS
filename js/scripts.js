b14130628fa5665ab32362cbfed1ac4dc3daeee6
Lisa Duschek <lisa.duschek@hotmail.com>
2015-09-11 10:00:03 -0700

    final styling and functioning code

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,34 @@
-var findReplace = function(input) {
+function FindReplace(input_sentence, replace_word, new_word) {
+  var input_new = input_sentence.replace(/\W+/g, ' ').toLowerCase().split(" ");
+  var replace_word = replace_word.toLowerCase();
+  var new_word = new_word.toLowerCase();
 
-var inputNew = input.replace(/\W+/g, '').replace(/\s+/g, '');
-var inputWord = inputNew;
+  input_new.forEach(function(word){
+      if (word === replace_word ){
+        input_new[input_new.indexOf(word)] = new_word;
 
-return inputWord;
+      }
+});
 
 
+  return input_new.join(" ").trim();
 
-};
+
+  }
+
+
+  $(document).ready(function()  {
+  $("form#find_and_replace").submit(function(event) {
+    var input_sentence = ($("input#input_sentence").val());
+    var replace_word = ($("input#replace_word").val());
+    var new_word = ($("input#new_word").val());
+    var result = FindReplace(input_sentence, replace_word, new_word);
+
+    $(".find_and_replace").text(result);
+
+    $("#result").show();
+    event.preventDefault();
+
+  });
+
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,18 @@
 describe('findReplace', function() {
 
-  it("finds one word in a string and outputs that word", function() {
+  it("takes one word input  and outputs that word", function() {
    expect(findReplace("banana")).to.equal("banana");
   });
+
+
+    it("takes two word input  and finds and outputs the word banana", function() {
+     expect(findReplace("another banana")).to.equal("banana");
+    });
+
+    it("takes string input  and finds and replaces the word banana with apple", function() {
+     expect(findReplace("another banana and many more")).to.equal("another apple and many more");
+    });
+
+
 
 });
