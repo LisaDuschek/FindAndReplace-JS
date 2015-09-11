1f8a0fae8eac8d44cb655d9f4543ad51d19c5c3a
Lisa Duschek <lisa.duschek@hotmail.com>
2015-09-11 09:04:13 -0700

    take one word input and output that word

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,10 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <link href="css/styles.css" rel="stylesheet" type="text/css">
-    <script src="js/jquery-1.11.3.js"></script>
-    <script src="js/scripts.js"></script>
-    <title>Find and Replace</title>
-  </head>
+var findReplace = function(input) {
 
-  <body>
+var inputNew = input.replace(/\W+/g, '').replace(/\s+/g, '');
+var inputWord = inputNew;
 
-    <div class="container">
-      <div class="jumbotron">
-        <h1>Waste your time-replace a word!</h1>
-      </div>
+return inputWord;
 
-      <form id="find_and_replace">
-        <div class="form-group">
-          <label for="input_string">Enter your sentence:</label>
-          <input id="input_string" type="text" class="form-control">
-          <label for="replace_word">Enter the word you want to be replaced:</label>
-          <input id="replace_word" type="text" class="form-control">
-          <label for="new_word">Enter the word you want to replace the old one with:</label>
-          <input id="new_word" type="text" class="form-control">
-        </div>
-        <button type-"submit" class="btn btn-success btn-lg" >Replace!</button>
-      </form>
 
-      <div id="result">
-        <h1><span class="find_and_replace"></span></h1>
-      </div>
 
-    </div>
-  </body>
-</html>
+};
---
+++ spec/specs.js
@@ -0,0 +1,7 @@
+describe('findReplace', function() {
+
+  it("finds one word in a string and outputs that word", function() {
+   expect(findReplace("banana")).to.equal("banana");
+  });
+
+});
